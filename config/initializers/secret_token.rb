df088feed9e00cca425fb1a59b81e248cda3da81
Eric A. Wood <eric.wood.ea@gmail.com>
2013-10-25 08:22:12 -0500

    First commit of a default rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsChallenge::Application.config.secret_token = '6b8fa7dcc9444129fdb8519257424ec26f48931b51203cc8bf89eb779855eca688eabd93561accf3db1f278221d83c901dc5b9452236163f5581d5b37a08c701'
