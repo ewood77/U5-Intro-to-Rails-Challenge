0ce2ee684b028e6a58435b2f26a3666112d05eae
Eric A. Wood <eric.wood.ea@gmail.com>
2013-10-25 09:21:50 -0500

    Pre-Bonus commit.

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ActiveRecord::Base
+  attr_accessible :description, :price, :stock_quantity, :title
+  
+  validates :title, :presence => true
+  validates :price, :presence => true
+  validates :stock_quantity, :presence => true
+  
+end
